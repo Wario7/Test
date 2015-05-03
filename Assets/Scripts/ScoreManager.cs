6ed9f4b878131a434f01191735913ccdd9c78439
Abdulaziz <ahomaidcmuq@gmail.com>
2015-05-03 20:37:11 +0300

    no message

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+
+	public int stayingAliveScore;
+	public static int score;        // The player's score.
+	Text text;                      // Reference to the Text component.
+	
+	
+	void Awake ()
+	{
+		// Set up the reference.
+		text = GetComponent <Text> ();
+		
+		// Reset the score.
+		score = 0;
+	}
+
+	void Start(){
+		InvokeRepeating("addScore", 1f, 1f);
+	}
+	
+	void Update ()
+	{
+		// Set the displayed text to be the word "Score" followed by the score value.
+		text.text = "Score: " + score;
+	}
+
+	void addScore(){
+		score += stayingAliveScore;
+	}
+
+}
--- Assets/Scripts/pop.cs
+++ Assets/Scripts/pop.cs
@@ -8,8 +8,9 @@ public class Pop : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.gameObject.tag == "Enemy") 
 		{
+			ScoreManager.score += 15;
 			Debug.Log ("hit");
-			AudioSource.PlayClipAtPoint(poppingSound, transform.position);	
+			AudioSource.PlayClipAtPoint(poppingSound, transform.position); //because GetComponent<AudioSource>().Play needs gameObject to be active for ~1.75 sec to playt the sound
 			gameObject.SetActive(false);
 			other.gameObject.SetActive(false);
 		}
